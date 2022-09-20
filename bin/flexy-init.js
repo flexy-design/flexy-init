a7f07d8d6543fce256d4579381900c43ed269079
blackmoja <limlim980625@gmail.com>
2022-09-21 02:20:28 +0900

    feat: flexy-init 설정파일 추가

---
+++ bin/flexy-init.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+console.log('👋 - welcome to flexy-init')
+console.log('👋 - flexy-init is a command that set the default settings for flexy.')
+console.log('👋 - We will support the function soon!')
